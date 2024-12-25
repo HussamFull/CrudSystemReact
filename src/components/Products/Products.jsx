8f278dc3d9e622f397c9cfc428cb1ad6329cee0b
hamada <bhak6150@gmail.com>
2024-12-25 23:39:23 +0100

    Products-Api

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState ,useEffect }  from 'react'
+import axios from 'axios';
+import useFetch from '../../assets/hooks/useFetch';
+import Loading from '../Loading/Loading';
+
+
 
 export default function Products() {
+
+
+  // Fetch data from API
+  const {data, isLoading , error} = useFetch('https://dummyjson.com/products');
+  
+  if (isLoading){
+     return <Loading />
+    }
   return (
-    <div>Products</div>
+    <>
+    {error? <div className="alert alert-danger">{error}</div>  : ''}  
+    <h1>Products</h1>
+    {data.products.map(product => 
+      <div key={product.id} >
+        <div >
+          {product.title}
+        </div>
+        <div className="card-body">
+          <p className="card-text">{product.description}</p>
+          <p className="card-text">{product.price}</p>
+        </div>
+      </div>
+    )}
+    </>
   )
 }
