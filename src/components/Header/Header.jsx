0d6d29f07b0beceede4415c5e213968320c534b0
hamada <bhak6150@gmail.com>
2024-12-27 02:37:44 +0100

    Header.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import './Header.css'
+
+
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+
+
+<div className="d-flex vh-100 justify-content-center align-items-center">
+      <header className="text-center">
+        <div className="header__container">
+          <h1 className="header__title">Welcome to Free Syria</h1>
+          <p className="header__subtitle">Free Syria, New Syria, Ancient Syria, Heritage Syria, Umayyad Syria, Syria of the people of the heavenly religions</p>
+        </div>
+      </header>
+    </div>
+
+    </>
   )
 }
